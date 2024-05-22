935a2c815b89af5918a20de1739387ff23acaf0c
SurfboardV2ray <125461738+Surfboardv2ray@users.noreply.github.com>
2024-05-21 22:10:43 -0700

    Update 2yt.py

--- 2yt.py
+++ 2yt.py
@@ -15,6 +15,8 @@ def download_youtube_video(url, output_path, quality=None):
     yt = YouTube(url)
     if quality:
         stream = yt.streams.filter(resolution=quality).first()
+        if not stream:
+            raise ValueError("Cannot find the specified resolution, please try again with a different option.")
     else:
         stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
     stream.download(output_path=output_path)
@@ -77,16 +79,20 @@ def button(update, context):
 
     if quality.isdigit():
         quality += 'p'  # Append 'p' to make it resolution (e.g., 720p)
-        query.answer()
-        query.edit_message_text(text=f"Selected video quality: {quality}")
-        url = context.user_data['url']
-        video_file = download_youtube_video(url, './', quality=quality)
-        fileio_link = upload_to_fileio(video_file)
-        if fileio_link:
-            query.message.reply_text(f'Here is your video in {quality}: {fileio_link}')
-        else:
-            query.message.reply_text('Failed to upload the video.')
-        os.remove(video_file)
+        try:
+            query.answer()
+            query.edit_message_text(text=f"Selected video quality: {quality}")
+            url = context.user_data['url']
+            video_file = download_youtube_video(url, './', quality=quality)
+            fileio_link = upload_to_fileio(video_file)
+            if fileio_link:
+                query.message.reply_text(f'Here is your video in {quality}: {fileio_link}')
+            else:
+                query.message.reply_text('Failed to upload the video.')
+            os.remove(video_file)
+        except ValueError as e:
+            query.answer()
+            query.edit_message_text(text=str(e))
     elif quality == 'lowest':
         query.answer()
         query.edit_message_text(text=f"Selected quality: Lowest Quality")
